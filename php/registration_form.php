f5ab4d8cb20006c3689e8de4a1af1e7d03521915
axelBaher <shiftmainsolo@gmail.com>
2022-06-03 20:05:21 +0300

    main

--- php/registration_form.php
+++ php/registration_form.php
@@ -8,22 +8,21 @@
     <title>Регистрация</title>
     <a name="top"></a>
 </head>
-<!-- btnSymbolUpCheck(); timer_reset(); colorPush(); -->
-<body onload="clearForm();">
+<!--   -->
+<body onload="clearForm(); btnSymbolUpCheck(); timer_reset(); colorPush();">
     <header>
         Сервис игровой статистики для геймеров
     </header>
     <article>
         <table id="form_table">
             <tbody>
-                
                 <tr>
                     <th>
                         <label id="label_name">Имя</label>
                     </th>
                     <td>
                         <input class="input" id="input_name" name=name
-                        onchange="inputDataEncoding(1)" onfocus="inputDataEncoding(1)" onselect="inputSelect(1)" 
+                        onchange="inputDataEncoding(1)"
                         type=text disabled autocomplete="off">
                     </td>
                     <td>
@@ -52,54 +51,40 @@
                     </td>
                 </tr>
                 <tr>
-                    <th><label id="label_num">Номер телефона</label></th>
-                    <td><input disabled onchange="inputDataEncoding(3)" onfocus="inputDataEncoding(3)" onselect="inputSelect(3)" 
-                        type=tel id="reg_num" name=phone_number placeholder="81234567890"></td>
+                    <th>
+                        <label id="label_num">Номер телефона</label>
+                    </th>
                     <td>
-                        <button class="btn" id="btn_symbol_down3" onclick="btnSymbolDown()"><</button>
-                                <label id="label_symbol3">~</label>
-                                <button class="btn" id="btn_symbol_up3" disabled onclick="btnSymbolUp()">></button>
-                                <button id="add_symbol3" onclick="btnAddSymbol()"></button>
+                        <input class="input" id="reg_num" name=phone_number 
+                        onchange="inputDataEncoding(3)" onfocus="inputDataEncoding(3)" onselect="inputSelect(3)"
+                        type=tel disabled placeholder="81234567890">
+                    </td>
+                    <td>
+                        <button class="btn" id="btn_symbol_down3" 
+                        onclick="btnSymbolDown()" onmouseover="hideSymbol()" onmouseout="showSymbol()">
+                        <</button>
+                        <label id="label_symbol3">~</label>
+                        <button class="btn" id="btn_symbol_up3"
+                        onclick="btnSymbolUp()" onmouseover="hideSymbol()" onmouseout="showSymbol()">
+                        ></button>
+                        <button id="add_symbol3" onclick="btnAddSymbol()"></button>
                     </td>
                 </tr>
                 <tr>
                     <th><label id="label_email">Email</label></th>
-                    <td><input disabled autocomplete="off" onchange="inputDataEncoding(4)" onfocus="inputDataEncoding(4)"
+                    <td><input class="input" disabled autocomplete="off" onchange="inputDataEncoding(4)" onfocus="inputDataEncoding(4)"
                             onselect="inputSelect(4)" type=email id="reg_email" name=email></td>
                     <td>
-                        <table align="center">
+                        <table>
                             <tr>
                                 <td><input type="color" id="colorMatch"></td>
                                 <td><button class="btn" id="btn_color" onclick="colorTimer()">Push me</button></td>
-                                <td><label id="label_colorToPick"></label></td>
+                                <td><input id="label_colorToPick"></input></td>
                             </tr>
                         </table>
                     </td>
                 </tr>
-                <tr id="row_to_hide1">
-                    <th>Любимые игры</th>
-                    <td colspan="2">
-                        <table id="checkbox_table">
-                            <tr>
-                                <td><input type=checkbox onchange="radioButtonOnChange(1)"
-                                        id="checkbox_fortnite">Fortnite</td>
-                            </tr>
-                            <tr>
-                                <td><input type=checkbox onchange="radioButtonOnChange(2)"
-                                        id="checkbox_league_of_legends">League of Legends</td>
-                            </tr>
-                            <tr>
-                                <td><input type=checkbox onchange="radioButtonOnChange(3)"
-                                        id="checkbox_overwatch">Overwatch</td>
-                            </tr>
-                            <tr>
-                                <td><input type=checkbox onchange="radioButtonOnChange(4)"
-                                        id="checkbox_pubg">PUBG</td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-                <tr id="row_to_hide2">
+                <tr>
                     <th>Откуда вы о нас узнали</th>
                     <td colspan="2">
                         <select onchange="selectOnChange()" name="how_hear_about_us" id="reg_heard">
@@ -125,4 +110,6 @@
     <script src="../js/phone_number.js"></script>
     <script src="../js/email.js"></script>
     <script src="../js/form.js"></script>
+
+    <script src="../js/script.js"></script>
 </html>
